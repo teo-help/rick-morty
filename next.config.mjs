29c2169eb3e6c9a634871e18068ffa87fb280df0
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-10-04 17:10:36 +0200

    test3

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     basePath: isProd ? '/rick-morty' : '',
+    output: 'export',
     assetPrefix: isProd ? '/rick-morty/' : '',
 };
 
