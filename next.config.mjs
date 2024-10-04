de22cad99e6d899b6843745105989f7c6eddb3e4
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-10-04 17:01:20 +0200

    test2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    basePath: isProd ? '/rick-morty' : '',
+    assetPrefix: isProd ? '/rick-morty/' : '',
+};
 
 export default nextConfig;
